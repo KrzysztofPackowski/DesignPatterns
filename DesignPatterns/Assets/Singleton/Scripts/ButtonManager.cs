500adef2353a57f37fe5242ae52fa3f0d320787b
Krzysztof Paćkowski <packowski_krzysztof@wp.pl>
2018-10-10 14:03:42 +0200

    Add Singleton example

---
+++ DesignPatterns/Assets/Singleton/Scripts/ButtonManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonManager : MonoBehaviour
+{
+    string _buttonText;
+
+    void Start()
+    {
+        _buttonText = this.transform.GetComponentInChildren<Text>().text.ToString();
+    }
+
+    public void TakeAction()
+    {
+        // Some button specific action
+        //...
+
+        Logger.Instance.Log("User clicked " + _buttonText);
+    }
+}
---
+++ DesignPatterns/Assets/Singleton/Scripts/Logger.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Logger
+{
+    // Private static refference to instance of this class
+    private static Logger _instance;
+
+    // Create an instance of this class only on first use
+    public static Logger Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = new Logger();
+            }
+            return _instance;
+        }
+    }
+
+    // Example logging method
+    public void Log(string log)
+    {
+        string date = DateTime.Now.ToString("yyyy-MM-dd h:mm:ss tt");
+        Debug.Log(date + ": " + log);
+    }
+
+    // Private constructor to prevent from creating new instances from outside this class
+    private Logger()
+    {
+        
+    }
+}
